a1bb00cd92c18218b5a80f5bb9218402f5dcacaf
Mantas Jurkuvenas <MantasJurkuvenas@gmail.com>
2023-02-21 07:25:41 +0000

    nice cmp

--- init.lua
+++ init.lua
@@ -261,48 +261,120 @@ mason_lspconfig.setup_handlers {
 -- Turn on lsp status information
 require('fidget').setup()
 
--- nvim-cmp setup
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-
-cmp.setup {
+---
+-- Autocomplete
+---
+require("luasnip.loaders.from_vscode").lazy_load()
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+local cmp = require("cmp")
+local source_mapping = {
+  luasnip = "",
+  cmp_tabnine = "",
+  nvim_lsp = "",
+  buffer = "﬘",
+  path = "",
+}
+local lspkind = require("lspkind")
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+local luasnip = require("luasnip")
+cmp.setup({
   snippet = {
+    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      luasnip.lsp_expand(args.body)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
   },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs( -4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+  sources = {
+    { name = "path" },
+    { name = "cmp_tabnine" },
+    { name = "nvim_lsp",   keyword_length = 1 },
+    { name = "buffer",     keyword_length = 3 },
+    -- { name = "vsnip" }, -- For vsnip users.
+    { name = "luasnip",    keyword_length = 2 },
+  },
+  window = {
+    completion = {
+      border = border("CmpBorder"),
+      winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+    documentation = {
+      border = border("CmpDocBorder"),
+    },
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      local menu = source_mapping[entry.source.name]
+      if entry.source.name == "cmp_tabnine" then
+        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+          menu = entry.completion_item.data.detail .. " " .. menu
+        end
+        vim_item.kind = ""
+      end
+      vim_item.menu = menu
+      return vim_item
+    end,
+  },
+  mapping = {
+    ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+    ["<Down>"] = cmp.mapping.select_next_item(select_opts),
+
+    ["<C-u>"] = cmp.mapping.scroll_docs( -4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable( -1) then
+    end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable( -1) then
         luasnip.jump( -1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
+    end, { "i", "s" }),
 
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      local col = vim.fn.col(".") - 1
+
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
