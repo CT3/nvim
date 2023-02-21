9fac4ccc6ca0cc2830e3d8554a9a4b840856e54a
Mantas Jurkuvenas <MantasJurkuvenas@gmail.com>
2023-02-21 07:33:04 +0000

    nice lualine

--- init.lua
+++ init.lua
@@ -53,13 +53,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Set lualine as statusline
 -- See `:help lualine.txt`
+local function search_count()
+  if vim.api.nvim_get_vvar("hlsearch") == 1 then
+    local res = vim.fn.searchcount({ maxcount = 100, timeout = 500 })
+
+    if res.total > 0 then
+      return string.format("%d/%d", res.current, res.total)
+    end
+  end
+
+  return ""
+end
+
 require('lualine').setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'gruvbox-flat',
-    component_separators = '|',
-    section_separators = '',
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
   },
+  sections = {
+    lualine_y = { "progress", { search_count, type = "lua_expr" } }
+  }
 }
 
 -- Enable Comment.nvim
